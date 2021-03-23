6ac960610e2911744f9077b657596737c0169286
TBSten <programmingCafeteria@gmail.com>
2021-03-23 23:31:11 +0900

    add new file

---
+++ ソースコードコピー.bat
@@ -0,0 +1,19 @@
+@echo off
+echo ●comへコピーします
+xcopy C:\Users\Wing_E_Valley\Documents\プログラミング\java\eclipceWorkSpace\プログラミング食堂\src D:\ドキュメント\[project]プログラミング食堂\_forgithub /D /S /R /Y /I /K
+
+echo ●git add を呼び出します
+git add *
+
+echo ●git commit　を呼び出します
+git commit -m "add new file"
+
+echo ●git status を呼び出します
+git status
+
+echo ●git remote を呼び出します
+git remote add origin https://github.com/TBSten/programmingCafeteria.git
+
+pause
+
+
